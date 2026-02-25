ef3c5c8584478d59c9b47af9ca93daa9e1aecbd1
Soumyadeep Chakraborty <soumyadeepchakraborty2006@gmail.com>
2026-02-25 10:54:16 +0530

    HackerRank Day 22

---
+++ pattern_arrrow.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%i",&n);
+    if (n<=1) {
+        printf("Shape Not Possible");
+    }
+    else {
+        for (int i=1;i<=n;i++) {
+            for (int j=1;j<=i;j++) {
+                if (j==1 || j==i) {
+                    printf("*");
+                }
+                else {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+        for (int i=n-1;i>=1;i--) {
+            for (int j=1;j<=i;j++) {
+                if (j==1 || j==i) {
+                    printf("*");
+                }
+                else {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
