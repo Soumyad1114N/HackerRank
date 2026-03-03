7a9e60ade66332f31aeef5e61ed3ae3b02564e2e
Soumyadeep Chakraborty <soumyadeepchakraborty2006@gmail.com>
2026-03-03 23:54:43 +0530

    HackerRank Day 23

---
+++ loops_FizzBuzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%i",&n);
+    for (int i=1;i<=n;i++) {
+        if (i%15==0) {
+            printf("FizzBuzz\n");
+        }
+        else if (i%5==0) {
+            printf("Buzz\n");
+        }
+        else if (i%3==0) {
+            printf("Fizz\n");
+        }
+        else {
+            printf("%i\n",i);
+        }
+    }
+    return 0;
+}
