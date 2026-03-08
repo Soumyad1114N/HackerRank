3e5366d34f39e402bbad29a140fbfa15f9915a0d
Soumyadeep Chakraborty <soumyadeepchakraborty2006@gmail.com>
2026-03-08 23:57:47 +0530

    HackerRank Day 24

---
+++ loop_sum_of_mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%i",&n);
+    int sum=0;
+    for (int i=1;i<=n;i++) {
+        if ((i%3==0)||(i%5==0)||(i%7==0)) {
+            sum+=i;
+        }
+    }
+    printf("%i",sum);
+    return 0;
+}
